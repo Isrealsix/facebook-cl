060e130f93659f86296e0b141f582bc5c8bd7739
Isreal6ix <isrealsix@gmail.com>
2021-08-21 13:47:01 +0700

    completed the header component

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,27 @@
 import Image from 'next/image';
+
+import {
+	BellIcon,
+	ChatIcon,
+	ChevronDownIcon,
+	HomeIcon,
+	UserGroupIcon,
+	ViewGridIcon,
+} from '@heroicons/react/solid';
+
+import {
+	FlagIcon,
+	PlayIcon,
+	SearchIcon,
+	ShoppingCartIcon,
+} from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
+
 const Header = () => {
 	return (
-		<div>
-			<h1>Header</h1>
-
-			<div>
+		<div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+			{/* Left */}
+			<div className="flex items-center">
 				<Image
 					src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
 					height={40}
@@ -12,6 +29,37 @@ const Header = () => {
 					layout="fixed"
 					alt="logo"
 				/>
+				<div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+					<SearchIcon className="h-6 text-gray-600" />
+					<input
+						type="text"
+						placeholder="Search Facebook"
+						className="hidden md:inline-flex ml-2 bg-transparent flex items-center outline-none placeholder-gray-500 flex-shrink"
+					/>
+				</div>
+			</div>
+
+			{/* Center */}
+
+			<div className="flex justify-center flex-grow">
+				<div className="flex space-x-6 md:space-x-2">
+					<HeaderIcon Icon={HomeIcon} active />
+					<HeaderIcon Icon={FlagIcon} />
+					<HeaderIcon Icon={PlayIcon} />
+					<HeaderIcon Icon={ShoppingCartIcon} />
+					<HeaderIcon Icon={UserGroupIcon} />
+				</div>
+			</div>
+
+			{/* Right */}
+			<div className="flex items-center sm:space-x-2 justify-end">
+				{/* Prof pic */}
+
+				<p className="whitespace-nowrap pr-3 font-semibold">Israel Ojeifo</p>
+				<ViewGridIcon className="icon" />
+				<ChatIcon className="icon" />
+				<BellIcon className="icon" />
+				<ChevronDownIcon className="icon" />
 			</div>
 		</div>
 	);
