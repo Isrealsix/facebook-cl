1beac6b1ff8080894c771766cda532796eb1f1c2
Isreal6ix <isrealsix@gmail.com>
2021-08-22 16:32:07 +0700

    completed the widget

---
+++ components/contacts.js
@@ -0,0 +1,15 @@
+const contacts = [
+	{ src: 'https:/links.papareact.com/f0p', name: 'Jeff Bezoz', id: 'fb-0' },
+	{ src: 'https:/links.papareact.com/kxk', name: 'Elon Musk', id: 'fb-091' },
+	{ src: 'https:/links.papareact.com/zvy', name: 'Bill Gates', id: 'fb-011' },
+	{
+		src: 'https:/links.papareact.com/snf',
+		name: 'Mark Zucherberg',
+		id: 'fb-110',
+	},
+	{ src: 'https:/links.papareact.com/d0c', name: 'Harry Potter', id: 'fb-120' },
+	{ src: 'https:/links.papareact.com/6gg', name: 'James Bond', id: 'fb-120' },
+	{ src: 'https:/links.papareact.com/r57', name: 'The Queen', id: 'fb-099' },
+];
+
+export default contacts;
